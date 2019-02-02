ef479f8fd7a438738a53ab285fd82e9fa8719c34
Daniel Thompson <1dptt@queensu.ca>
2019-02-02 13:11:33 -0500

    mostly working now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,6 +16,8 @@ var guessCounter = document.querySelector("#guessCounter");
 
 // FUNCTIONS;
 // =======================================================
+
+
 function generateWordSoFar(){
     for(var i = 0; i < randomWord.length; i++){
         wordSoFar.push('_');
@@ -23,10 +25,26 @@ function generateWordSoFar(){
     return wordSoFar;
 }
 
+function playerWin(){
+
+    if(wordSoFar.join('') == randomWord){
+       wins = wins + 1;
+       winCounter.innerHTML =("Wins: " + wins);
+       console.log(wordSoFar.join(''));   
+       console.log(wins);
+       alert("win");
+
+       
+       }
+   }
+
+
 function init(){
-    guessesRemaining = randomWord.length * 2
-    currentWord.innerHTML = generateWordSoFar().join(' ');    
+    guessesRemaining = randomWord.length * 2;
+    currentWord.innerHTML = generateWordSoFar().join(' ');
 }
+
+
 
 
 
@@ -36,25 +54,41 @@ init();
 // MAIN;
 // =======================================================
 document.addEventListener('keypress', (event) => {
+    var restart= false;
     var key = String.fromCharCode(event.keyCode);
    
     guessesRemaining = guessesRemaining - 1;
-    guessCounter.innerHTML = guessesRemaining;
+    guessCounter.innerHTML =("Remaining Guesses: "  + guessesRemaining);
     //if users guess is right
     if(randomWord.indexOf(key) > -1) {
-
+        
     //replace currentWord with right letter
         wordSoFar[randomWord.indexOf(key)] = key;
         currentWord.innerHTML = wordSoFar.join(' ');
-
-      //  console.log(wordSoFar);
-        
+        playerWin();
+        if(wordSoFar == randomWord){
+            restart=true;
+        }
 
     } else {
         wrongGuesses.push(key);
         console.log(wrongGuesses); 
-        wrongBank.innerHTML = wrongGuesses.join(", ");  
+        wrongBank.innerHTML =("Wrong Guesses: " + wrongGuesses.join(", "));
+          
     }
+    if(wordSoFar.join('') != randomWord 
+        && guessesRemaining == 0){
+        alert("game over");
+        restart=true;
+        }
+ 
+  if(restart === true){
+    randomWord; 
+    guessesRemaining = randomWord.length * 2;
+  guessCounter.innerHTML = ("Remaining guesses: " + guessesRemaining);
+  wrongBank.innerHTML = "Wrong guesses: ";
+
+  }
 });
 
 
