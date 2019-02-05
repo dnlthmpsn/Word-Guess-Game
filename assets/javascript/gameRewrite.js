f6f78bc9a393a2f51fc66a1ba9bf313b6ceb4f1f
Daniel Thompson <1dptt@queensu.ca>
2019-02-05 14:17:45 -0500

    improved but still not working

--- assets/javascript/gameRewrite.js
+++ assets/javascript/gameRewrite.js
@@ -1,14 +1,21 @@
-var setUp,
+var s,
 
 
 WordGuess = {
 
     setUp: {
         wordList: ["leonard", "lowry", "carter", "bosh", "derozan"],
+        randomWord: function(){
+            var randomWord =(wordList[Math.floor(Math.random() * wordList.length)])
+            return randomWord;
+    },
+
+
+        
         wins: 0,
         wrongGuesses: [],
         wordSoFar: [],
-        randomWord: wordList[Math.floor(Math.random() * wordList.length)],
+        randomWord: [],
         guessesRemaining: (randomWord.length * 2),
         restart: false,
     },
@@ -17,39 +24,45 @@ WordGuess = {
     init: function(){
 
         s = this.setUp;
+        this.randomWord;
         this.generateWordsoFar();
         this.playerWin();
-        guessesRemaining = randomWord.length * 2;
+        guessesRemaining = (randomWord.length * 2);
         currentWord.innterHTML = generateWordsoFar().join(' ');
-
+        this.keyPress();
     },
 
 
     //binding UI elements
-    listenToKeys: function(){ 
+   keyPress: function(){ 
         document.addEventListener('keypress', function(event){
             var key = String.fromCharCode(event.keyCode);
             guessesRemaining = guessesRemaining - 1;
-            guessCounter.innerHTML =("remaining guesses: " + guessesRemaining);
-            //if user guess is right
-            if(randomWord.indexOf(key) > -1){
-                wordSoFar[randomWord.indexOf(key)] = key;
-                currentWord.innerHTML = wordSoFar,join(' ');
-                playerWin();
-                if(wordSoFar === randomWord){
-                    restart = true;    
-                } else {
-                        wrongGuesses.push(key);
-                        wrongBank.innerHTML = ("wrong guesses " + wrongGuesses.join(", "));            
-                }
+            guessCounter.innerHTML = ("remaining guesses: " + guessesRemaining);
+        })
+    },
+
+        
+    correctGuess: function(){
+        if (randomWord.indexof(ley) > -1){
+            wordSoFar[randomWord.indexOf(key)] = key;
+            correntWord.innerHTML = wordsoFar,join(' ');
         }
-        if(ordSoFar.join('') != randomWord && guessesRemaining == 0{
-            alert"game over");
+    },
+
+    guessHandler: function(){
+        if (wordSoFar === randomWord){
+            restart = true;
+        } else {
+            wrongGuess.push(key);
+            wrongBank.innerHTML = ("wrong guesses " + wrongGuess.join(", "));
+        }
+        if(orderSoFar.join('') != randomWord && guessesRemaining == 0){
+            alert("game over");
             restart = true;
         }
-
-    },    
-   
+    },
+    
     generateWordSoFar: function(){
         for( var i = 0; i <randomWord.length; i++){
             wordSoFar.push('_');
@@ -58,7 +71,8 @@ WordGuess = {
     },
 
     playerWin: function(){
-        if(wordSoFar.join('' == randomWord){
+        if(wordSoFar.join('') == randomWord){
+
             wins = wins + 1;
             winCounter.innerHTML =("wins: " + wins);
             restart = true;
